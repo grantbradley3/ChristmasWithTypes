8ab9b54174577839c94fdff2959cecc9e80e1faf
Grant Bradley <grantbradleytiger13@gmail.com>
2022-11-16 20:19:04 -0500

    fixed missing info

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks.Dataflow;
 
 namespace ChristmasWithTypes
 {
@@ -11,13 +10,13 @@ namespace ChristmasWithTypes
 
             var xmasDay = Christmas.Day.Thursday;
 
-            //TODO set Santa's name to Kris Kringle
-            xmas.Santa = Kris Kringle;
+            //done set Santa's name to Kris Kringle
+            xmas.Santa = "Kris Kringle";
 
-            //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{books,food,knowledge }; 
+            //done Insert 3 presents you would like for xmas.  They must be strings
+            xmas.Presents =  new string[3] { "books", "food", "shoes"}; 
 
-            //TODO Set the TreeHeight to 10
+            //done Set the TreeHeight to 10
             xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
